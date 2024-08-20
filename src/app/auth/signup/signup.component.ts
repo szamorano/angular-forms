5b6f36fc8a217357f3475eca2a284469f14957ac
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-20 13:55:21 -0400

    Working with form arrays

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -22,28 +22,31 @@ export class SignupComponent {
         validators: [Validators.required, Validators.minLength(6)],
       }),
     }),
-    
-    firstName: new FormControl('', { validators: [Validators.required]}),
-    lastName: new FormControl('', { validators: [Validators.required]}),
+
+    firstName: new FormControl('', { validators: [Validators.required] }),
+    lastName: new FormControl('', { validators: [Validators.required] }),
 
     address: new FormGroup({
-      street: new FormControl('', { validators: [Validators.required]}),
-    number: new FormControl('', { validators: [Validators.required]}),
-    postalCode: new FormControl('', { validators: [Validators.required]}),
-    city: new FormControl('', { validators: [Validators.required]}),
+      street: new FormControl('', { validators: [Validators.required] }),
+      number: new FormControl('', { validators: [Validators.required] }),
+      postalCode: new FormControl('', { validators: [Validators.required] }),
+      city: new FormControl('', { validators: [Validators.required] }),
     }),
-    
-    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {validators: [Validators.required] }),
+
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', { validators: [Validators.required] }),
+    source: new FormArray([
+      new FormControl(false),
+      new FormControl(false),
+      new FormControl(false),
+    ]),
     agree: new FormControl(false, { validators: [Validators.required] }),
   });
 
-  onSubmit()
-  {
+  onSubmit() {
     console.log(this.form)
   }
 
-  onReset()
-  {
+  onReset() {
     this.form.reset();
   }
 }
