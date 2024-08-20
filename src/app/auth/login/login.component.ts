c18530aaabb78e6ccdcef8bd2b5960039f5725ca
Steven Zamorano <stevenzamorano.code@gmail.com>
2024-08-20 09:41:17 -0400

    Storing email entered in Localstorage using the underlying form object

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,18 +10,31 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
 
-  onSubmit(formData: NgForm)
-  {
-    if(formData.form.invalid)
-    {
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.email })
+        ),
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    })
+  }
+
+  onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
       return;
     }
-    
+
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
 
     console.log(formData.form)
     console.log(enteredEmail, enteredPassword)
+
+    formData.form.reset();
   }
 }
